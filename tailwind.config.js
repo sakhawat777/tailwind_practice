39b9b777e5684a66d4d8c6b757bc5337177cb920
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-09-05 15:16:31 +0600

    Device Preference Dark Mode Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js}'],
+	// For Device Preference Dark Mode use media or App Button Preference Dark Mode use class
+	darkMode: 'media',
 	theme: {
 		extend: {},
 	},
